01002b6ab4a25714e19c7b97ef35ae6f7322ca53
Cole Willsea <coleww@gmail.com>
2015-11-04 05:25:35 -0800

    add list of ableist language

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 
-var isCool = require('iscool')()
+var isCool = require('iscool')({  tragedyHappenedRecently: true,
+  customBlacklist: [
+  'cry', 'crying', 'tears', 'loss',
+    'sad',
+    'tragedy',
+    'barren',
+    'blind',
+    'bound',
+    'crazy',
+    'cripple',
+    'crippled',
+    'daft',
+    'deaf',
+    'derp',
+    'dumb',
+    'feeble',
+    'handicap',
+    'handicapped',
+    'idiot',
+    'imbecile',
+    'insane',
+    'invalid',
+    'lame',
+    'loon',
+    'lunatic',
+    'madhouse',
+    'mental',
+    'mongoloid',
+    'manic',
+    'nuts',
+    'moron',
+    'psycho',
+    'retard',
+    'spaz',
+'stupid'
+
+
+
+ ] })
 
 module.exports = function (str) {
   return str.split('\W').every(function (word) {
